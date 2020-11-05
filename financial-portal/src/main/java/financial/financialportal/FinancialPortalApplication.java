d7967c8d3ae2490a7496c4522b2b21dab931b86a
Amir Mustafaa <amirmustafaa1@gmail.com>
2020-11-04 19:37:56 -0500

    Initial Commit

---
+++ financial-portal/src/main/java/financial/financialportal/FinancialPortalApplication.java
@@ -0,0 +1,13 @@
+package financial.financialportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPortalApplication.class, args);
+	}
+
+}
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return(
+    <Router>
+      <Switch>
+      </Switch>
+
+    </Router>
+
+
+  );
+}
+
+export default App;
