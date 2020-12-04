73a1d8391304ed31af04b9d288741226d232452c
Amir Mustafaa <amirmustafaa1@gmail.com>
2020-12-03 22:21:04 -0500

    Deploy Website

--- financial-portal/src/main/java/com/amirmustafaa/financialportal/FinancialPortalApplication.java
+++ financial-portal/src/main/java/com/amirmustafaa/financialportal/FinancialPortalApplication.java
@@ -2,8 +2,10 @@ package com.amirmustafaa.financialportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class FinancialPortalApplication {
 
 	public static void main(String[] args) {
