deda729f7584b2154d4d1f02e094bf8ab91eab4b
Amir Mustafaa <amirmustafaa1@gmail.com>
2020-12-04 17:02:27 -0500

    Deploy Website

--- financial-portal/src/main/java/com/amirmustafaa/financialportal/security/WebSecurityConfig.java
+++ financial-portal/src/main/java/com/amirmustafaa/financialportal/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+//
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
 //		http.cors().and().csrf().disable()
@@ -62,4 +62,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 //	}
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	                .authorizeRequests()
+	                .antMatchers("/**").permitAll();
+	    }
 }
